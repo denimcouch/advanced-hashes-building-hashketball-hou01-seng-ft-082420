df39232eb1e25bcfd0e3174a832c3910478a403b
Alex Mata <alexmata92@ymail.com>
2020-08-07 20:02:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,17 @@
 # Write your code here!
 def game_hash
+  game = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [{}]
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [{}]
+    }
+  }
   
+  game
 end
